2d10889ed4a6ddbb282bd72361071298adce94ea
HafizhMA <muhammadazhr29@gmail.com>
2023-09-20 20:19:42 +0700

    menampilkan userdata

--- src/App.jsx
+++ src/App.jsx
@@ -30,45 +30,60 @@ function App() {
           <h1 className="text-center font-bold">input data user</h1>
           <div className="form-container">
             <form>
-              <div class="mb-4">
-                <label class="block text-sm font-bold mb-2" for="nama">
+              <div className="mb-4">
+                <label className="block text-sm font-bold mb-2" for="nama">
                   nama
                 </label>
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="nama"
                   type="text"
                   placeholder="nama"
                 />
               </div>
-              <div class="mb-4">
-                <label class="block text-sm font-bold mb-2" for="umur">
+              <div className="mb-4">
+                <label className="block text-sm font-bold mb-2" for="umur">
                   umur
                 </label>
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="umur"
                   type="number"
                   placeholder="umur"
                 />
               </div>
-              <div class="mb-4">
-                <label class="block text-sm font-bold mb-2" for="jenis-kelamin">
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-bold mb-2"
+                  for="jenis-kelamin"
+                >
                   jenis-kelamin
                 </label>
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="jenis-kelamin"
                   type="text"
                   placeholder="jenis-kelamin"
                 />
               </div>
-              <div class="mb-4">
+              <div className="mb-4">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   Submit
                 </button>
               </div>
             </form>
+          </div>
+          <div className="output-Container">
+            {dataUser.map((user) => (
+              <div
+                className="user-data mb-4 bg-slate-200 text-slate-900 rounded-sm p-3"
+                key={user.id}
+              >
+                <p>nama: {user.nama}</p>
+                <p>umur: {user.umur}</p>
+                <p>jenis-kelamin: {user.jeniskelamin}</p>
+              </div>
+            ))}
           </div>
         </div>
       </section>
